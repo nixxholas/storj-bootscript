2bd0f28d3bc919d2e498f9a360936575795a9cc2
Nicholas <nixholas@icloud.com>
2020-02-09 01:11:06 +0800

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -1,4 +1,20 @@
-apt install -y unzip zip curl
+apt install -y unzip zip curl apt-transport-https \
+    ca-certificates \
+    curl \
+    gnupg-agent \
+    software-properties-common
+    
+curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo apt-key add -
+apt-key fingerprint 0EBFCD88
+add-apt-repository \
+   "deb [arch=amd64] https://download.docker.com/linux/ubuntu \
+   $(lsb_release -cs) \
+   stable"
+apt update
+apt install -y docker-ce docker-ce-cli containerd.io
+
+docker pull storjlabs/watchtower
+docker pull storjlabs/storagenode:beta
 
 curl -L https://github.com/storj/storj/releases/latest/download/identity_linux_amd64.zip -o identity_linux_amd64.zip
 unzip -o identity_linux_amd64.zip
